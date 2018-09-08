0f20129dfc8f20f8e2dc5b672d2c292c43e0c774
Oleg <move.1668@gmail.com>
2018-09-08 15:58:36 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+
+// var theShop = prompt('Are your shop?', 'Your answer.');
+// var nameShop = prompt('What is the name of your shop?', 'Your answer.');
+
+// var shopGoods = [];
+// shopGoods[0] = prompt('What is be sell?', 'Product.');
+// shopGoods[1] = prompt('What is be sell?', 'Product.');
+// shopGoods[2] = prompt('What is be sell?', 'Product.');
+
+var mainList = {
+	budget: 210120,
+	// nameShop: nameShop,
+	// shopGoods: shopGoods,
+	employers: {
+		programmer: 'John',
+		webDeziner: 'Luci'
+	},
+	open: true
+}
+
+console.log(mainList.budget / 12 / 30);
+
+// console.log(mainList.budget);
+// console.log(mainList.nameShop);
+// console.log(mainList.shopGoods);
+// console.log(mainList.employers);
+// console.log(mainList.open);
